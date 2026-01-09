87e3bc4576d39a5a9639483f473eb42aae561dab
Software Shinobi <the.software.shinobi@gmail.com>
2026-01-09 18:15:43 -0500

    update files

--- provision.bash
+++ provision.bash
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+set -e
+
+set -x
+
+##
+
+apt update
+
+##
+
+apt install pip -y
+
+##
+
+pip install mkdocs-material
+
+##
+
 pip install mkdocs-with-pdf --break-system-packages
 
 pip install mkdocs-to-pdf
